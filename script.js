f0fd6f32b219bcf700f42dc144466d1dc1de4b59
Nahid1911 <nahid.raihan>
2023-04-10 22:15:07 +0600

    Stylelint and ESLint correction

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const mobileMenu = document.getElementById('mobile-menu');
 function displayFunction() {
   const menuList = document.getElementById('nav-menu');
   if (menuList.classList.contains('menue-button-pressed') === false) {
-    menuList.classList.add('menue-button-pressed'); }
-  else { menuList.classList.remove('menue-button-pressed') }
+    menuList.classList.add('menue-button-pressed');
+} else { menuList.classList.remove('menue-button-pressed'); }
   const menuIcon = document.getElementById('mobile-menu');
   const menuIconArrtibute = menuIcon.getAttribute('src');
   const image1 = 'media/Hamberger_menu_icon.svg';
   const image2 = 'media/Cancel-Menu-icon.svg';
-const newSer = (menuIconArrtibute === image1) ? image2 : image1;
+  const newSer = (menuIconArrtibute === image1) ? image2 : image1;
   menuIcon.setAttribute('src', newSer);
 }
-  mobileMenu.addEventListener('click', displayFunction);
+mobileMenu.addEventListener('click', displayFunction);
