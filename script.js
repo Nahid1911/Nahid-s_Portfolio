75485bce82c0a4db81128e4ff8574c64e4e285ef
Nahid1911 <nahid.raihan>
2023-04-10 21:57:51 +0600

    Styling Error correction

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let mobileMenu = document.getElementById("mobile-menu");
-mobileMenu.addEventListener("click", displayFunction);
-
+const mobileMenu = document.getElementById('mobile-menu');
 
 function displayFunction() {
-let menuList = document.getElementById("nav-menu")
-if(menuList.classList.contains("menue-button-pressed") === false){
-    menuList.classList.add("menue-button-pressed")
-}else {menuList.classList.remove("menue-button-pressed")}
-let menuIcon = document.getElementById("mobile-menu");
-let menuIconArrtibute = menuIcon.getAttribute("src");
-let image1 = "media/Hamberger_menu_icon.svg"
-let image2 = "media/Cancel-Menu-icon.svg"
-let newSer = (menuIconArrtibute === image1) ? image2:image1;
-menuIcon.setAttribute("src",newSer);
+  const menuList = document.getElementById('nav-menu');
+  if (menuList.classList.contains('menue-button-pressed') === false) {
+    menuList.classList.add ('menue-button-pressed');
+  }
+  else { menuList.classList.remove('menue-button-pressed') };
+  const menuIcon = document.getElementById('mobile-menu');
+  let menuIconArrtibute = menuIcon.getAttribute('src');
+  let image1 = 'media/Hamberger_menu_icon.svg';
+  let image2 = 'media/Cancel-Menu-icon.svg';
+const newSer = (menuIconArrtibute === image1) ? image2 : image1;
+menuIcon.setAttribute('src', newSer);
 }
+mobileMenu.addEventListener('click', displayFunction);
