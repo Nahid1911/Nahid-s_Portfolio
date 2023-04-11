e2eaeb9a7c934d5d35d132df486b97c2a6230695
Nahid1911 <nahid.raihan>
2023-04-11 11:44:13 +0600

    mobile menu update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//for mobile menu appear and disappear
 const mobileMenu = document.getElementById('mobile-menu');
 
 function displayFunction() {
@@ -13,3 +15,20 @@ function displayFunction() {
   menuIcon.setAttribute('src', newSer);
 }
 mobileMenu.addEventListener('click', displayFunction);
+
+// Menu disapear when click on any Item on the menu
+
+const menuItemPortfolio = document.getElementById('portfolio-li');
+const menuItemAbout = document.getElementById('about-li');
+const menuItemContact = document.getElementById('contact-li');
+menuItemPortfolio.addEventListener('click', menuDisappear)
+menuItemAbout.addEventListener('click', menuDisappear)
+menuItemContact.addEventListener('click', menuDisappear)
+
+function menuDisappear() {
+  const menuList = document.getElementById('nav-menu');
+  menuList.classList.remove('menue-button-pressed');
+  document.getElementById('mobile-menu').setAttribute('src', 'media/Hamberger_menu_icon.svg');
+}
+
+
