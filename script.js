d52f8436babb40856e01529899a71551acb53f3f
Nahid1911 <nahid.raihan>
2023-04-11 20:37:53 +0600

    Popup window add

--- script.js
+++ script.js
@@ -30,3 +30,10 @@ function menuDisappear() {
 menuItemPortfolio.addEventListener('click', menuDisappear);
 menuItemAbout.addEventListener('click', menuDisappear);
 menuItemContact.addEventListener('click', menuDisappear);
+
+let projectInfo1 = {
+  projectName: 'Keeping track of hundreds of components',
+   projectDescriptionP1: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.',
+   projImage: "", projTechno1: "Ruby on Rails", projTechno2: "CSS", projTechno3: "JavaScript",
+   linkToLive: "", linkToSource: ""
+}
