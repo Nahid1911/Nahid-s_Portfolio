e4ceb0b4276689d8ab8a793a6d07883c81614fa4
Nahid1911 <nahid.raihan>
2023-04-12 23:59:54 +0600

    add mobile and desktop popup window without using object

--- script.js
+++ script.js
@@ -34,10 +34,10 @@ menuItemContact.addEventListener('click', menuDisappear);
 //  ----Mobile Popup Window ----
 
 // let projectInfo1 = {
-//   projectName: 'Keeping track of hundreds of components',
+//    projectName: 'Keeping track of hundreds of components',
 //    projectDescriptionP1: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.',
 //    projImage: '', projTechno1: 'Ruby on Rails', projTechno2: 'CSS', projTechno3: 'JavaScript',
-//    linkToLive: '', linkToSource: ''
+//    linkToLive: 'https://nahid1911.github.io', linkToSource: 'https://github.com/Nahid1911/Nahid-s_Portfolio'
 // }
 
 let firstProjectBtn = document.getElementById('project-1');
